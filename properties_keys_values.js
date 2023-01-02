4239c60644df0ac3f1377897827d9f2b3baf5120
Abdullah Ibn Omor Shahrin <abdullahshahrin4.67@gmail.com>
2023-01-02 16:37:11 +0600

    properties

---
+++ properties_keys_values.js
@@ -0,0 +1,35 @@
+var shoppingcart = {
+    mouse: 3,
+    display: 2,
+    pen: 5
+}
+console.log(shoppingcart);
+
+//set properties values
+shoppingcart.mouse = 5;
+console.log(shoppingcart);
+
+shoppingcart['mouse'] = 7;
+console.log(shoppingcart);
+
+var property_name = 'mouse';
+shoppingcart[property_name] = 9;
+console.log(shoppingcart);
+
+//get property value
+var property_value1 = shoppingcart.pen;
+console.log(property_value1);
+
+var property_value_2 = shoppingcart['display'];
+console.log(property_value_2);
+
+var property_name = 'mouse';
+var property_value = shoppingcart[property_name];
+console.log(property_name, property_value);
+
+
+var properties = Object.keys(shoppingcart);
+console.log(properties);
+
+var property_values = Object.values(shoppingcart);
+console.log(property_values);
