1697ae09e3fcd515febedad4fca7e137528cfbc2
Abdullah Ibn Omor Shahrin <abdullahshahrin4.67@gmail.com>
2023-01-09 01:00:31 +0600

    ternaryOperator

---
+++ ternaryOperator.js
@@ -0,0 +1,7 @@
+// alternative of if-else condition
+var age = 18;
+
+// condition? expression if true : expression if false
+
+var checkAge = (age <= 18) ? "child" : "adult";
+console.log(checkAge);
