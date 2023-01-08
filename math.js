4d55c7e1852c600353f42ce6a4a86c8a2dccbbb7
Abdullah Ibn Omor Shahrin <abdullahshahrin4.67@gmail.com>
2023-01-09 00:46:05 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,23 @@
+const result = Math.pow(3, 8); // 3 to the power 8, find the power
+console.log(result);
+
+var num1 = 5;
+var num2 = 10;
+const differ = num1 - num2;
+console.log(differ);
+
+const absoluteDiffer = Math.abs(num1 - num2);
+console.log(absoluteDiffer);
+
+const num3 = 2.34;
+const result3 = Math.ceil(num3);
+const result4 = Math.floor(num3);
+const result5 = Math.round(num3); // balace with .5
+console.log(result3, result4, result5);
+
+
+const randomNum = Math.random() * 100; ////between 100
+console.log('random number ' + randomNum);
+
+const randomNumRound = Math.round(Math.random() * 6); //between 6
+console.log('random number rounded ' + randomNumRound);
