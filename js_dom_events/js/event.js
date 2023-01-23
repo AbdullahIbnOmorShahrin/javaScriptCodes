69c693583c98e75a0052e862c7a61861f92a9a32
Abdullah Ibn Omor Shahrin <abdullahshahrin4.67@gmail.com>
2023-01-23 23:22:04 +0600

    first commit

--- js_dom_events/js/event.js
+++ js_dom_events/js/event.js
@@ -1,1 +1,15 @@
 console.log('this is seperate js file');
+function makeRed() {
+    document.body.style.backgroundColor = "red";
+}
+
+const makePurpleButton = document.getElementById('make-purple');
+makePurpleButton.onclick = makePurple;
+function makePurple() {
+    document.body.style.backgroundColor = "purple";
+}
+
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = function makeBlue() {
+    document.body.style.backgroundColor = "blue";
+};
