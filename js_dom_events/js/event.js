29fb2b7f4a564d38a0e5f89d96fa8bf9d9eef451
Abdullah Ibn Omor Shahrin <abdullahshahrin4.67@gmail.com>
2023-01-25 13:13:44 +0600

    first commit

--- js_dom_events/js/event.js
+++ js_dom_events/js/event.js
@@ -2,13 +2,13 @@ console.log('this is seperate js file');
 //option 1
 function makeRed() {
     document.body.style.backgroundColor = "red";
-}
+};
 //option 2
 const makePurpleButton = document.getElementById('make-purple');
 makePurpleButton.onclick = makePurple;
 function makePurple() {
     document.body.style.backgroundColor = "purple";
-}
+};
 //option 3
 const makeBlueButton = document.getElementById('make-blue');
 makeBlueButton.onclick = function makeBlue() {
@@ -19,7 +19,7 @@ const makePinkButton = document.getElementById('make-pink');
 makePinkButton.addEventListener('click', makePink);
 function makePink() {
     document.body.style.backgroundColor = "pink";
-}
+};
 //option 5
 const makeGreenButton = document.getElementById('make-green');
 makeGreenButton.addEventListener('click', function makeGreen() {
@@ -28,4 +28,22 @@ makeGreenButton.addEventListener('click', function makeGreen() {
 //option 6
 document.getElementById('make-gold').addEventListener('click', function () {
     document.body.style.backgroundColor = 'gold';
+});
+//option 7
+function handleOnClick() {
+    const defaultText = document.getElementById('default-text');
+    defaultText.innerText = 'Change by onclick function';
+};
+//option 8
+document.getElementById('event-listner').addEventListener('click', function () {
+    const defaultText = document.getElementById('default-text');
+    defaultText.innerText = 'Change by event listener';
+});
+//option 9
+document.getElementById('update-text').addEventListener('click', function () {
+    const inputedText = document.getElementById('inputed-text');
+    const defaultText = document.getElementById('default-text');
+    defaultText.innerText = 'Change by event listener';
+    defaultText.innerText = inputedText.value;
+    inputedText.value = '';
 });
